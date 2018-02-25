854f7c68e0478b977736715afc8924317bee1dfd
Unknown <r.raynsford@gmail.com>
2018-02-25 23:54:37 +0000

    Add webpack

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # js_scaffold
+A pre-configured Javascript environment to provide a quick starting point for projects.
+
+## Environment includes:
+
+*[Editor Config](http://editorconfig.org/, "Editor config")
+*[ESLint](https://eslint.org/ "ESLint") (Follows airbnb-base)
+*[TernJS](https://ternjs.net/, "TernJS")
+*[Webpack](https://webpack.js.org/, "Webpack")
+*[BabelJS](https://babeljs.io/, "Babel")
+*[UglifyJS](https://github.com/mishoo/UglifyJS, "UglifyJS")
+
+## Installation
+Clone the repo, run npm install and then npm start.
+
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Uglify = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 9000,
+  },
+  resolve: {
+    extensions: ['.js', '.css', '.scss', '.png', '.jpg', '.gif'],
+  },
+  module: {
+    loaders: [
+      { test: /\.s?css$/, loader: ['style-loader', 'css-loader', 'sass-loader'] },
+      { test: /\.(png|jpg|gif)$/, loader: 'url-loader' },
+      { test: /\.js$/, use: { loader: 'babel-loader', options: { presets: ['env'] } } },
+    ],
+  },
+  plugins: [
+    new Uglify(),
+    new HtmlWebpackPlugin({
+      hash: true,
+      template: './src/index.html',
+      path: path.join(__dirname, 'dist'),
+      filename: 'index.html',
+    }),
+  ],
+};
